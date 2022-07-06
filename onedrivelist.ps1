9d3112818192ec7fd1d7a53e5e03028adfe7ae92
moram87 <81384088+moram87@users.noreply.github.com>
2022-07-06 16:30:29 -0500

    Add files via upload

---
+++ onedrivelist.ps1
@@ -0,0 +1,5 @@
+$TenantUrl = "https://nationalrrmuseum-admin.sharepoint.com/"
+$LogFile = [Environment]::GetFolderPath("Desktop") + "\OneDriveSites.log"
+Connect-SPOService $TenantUrl
+Get-SPOSite -IncludePersonalSite $true -Limit all -Filter "Url -like '-my.sharepoint.com/personal/'" | Select -ExpandProperty Url | Out-File $LogFile -Force
+Write-Host "Done! File saved as $($LogFile)."
