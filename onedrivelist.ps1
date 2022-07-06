8f016adfde7f8a56a082e86b47dff9a8984a555b
moram87 <81384088+moram87@users.noreply.github.com>
2022-07-06 16:31:08 -0500

    Update onedrivelist.ps1

--- onedrivelist.ps1
+++ onedrivelist.ps1
@@ -1,4 +1,4 @@
-$TenantUrl = "https://nationalrrmuseum-admin.sharepoint.com/"
+$TenantUrl = "https://tenant-name.sharepoint.com/"
 $LogFile = [Environment]::GetFolderPath("Desktop") + "\OneDriveSites.log"
 Connect-SPOService $TenantUrl
 Get-SPOSite -IncludePersonalSite $true -Limit all -Filter "Url -like '-my.sharepoint.com/personal/'" | Select -ExpandProperty Url | Out-File $LogFile -Force
